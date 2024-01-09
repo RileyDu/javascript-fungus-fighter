b5acb38210eeab1b42860ce6327291e8881e3364
Riley DuBord <thelifeofdu@gmail.com>
2024-01-08 19:39:41 -0600

    progress bars now update with each attack

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ let fungusAP = 100;
 let myAPElement = document.getElementsByClassName("ap-text")[0];
 let yourHPElement = document.getElementsByClassName("hp-text")[0];
 // console.log(yourHPElement, myAPElement);
+let hpMeter = document.getElementById('hp-meter')
+let apMeter = document.getElementById('ap-meter')
 
 function onReady() {
   console.log("Ready to go!");
@@ -46,6 +48,8 @@ function fireScepter(event) {
   console.log("Fire Scepter!");
   fungusHP -= 14;
   fungusAP -= 12;
+  hpMeter.value -= 14;
+  apMeter.value -= 12;
   if (fungusAP < 0) {
     fungusAP = 0;
   }
@@ -63,6 +67,8 @@ function fireEntagle(event) {
   console.log("Fire Entagle!");
   fungusHP -= 9;
   fungusAP -= 23;
+  hpMeter.value -= 9;
+  apMeter.value -= 23;
   if (fungusAP < 0) {
     fungusAP = 0;
   }
@@ -79,6 +85,8 @@ function fireBlade(event) {
   console.log("Fire Blade!");
   fungusHP -= 47;
   fungusAP -= 38;
+  hpMeter.value -= 47;
+  apMeter.value -= 38;
   if (fungusAP < 0) {
     fungusAP = 0;
   }
@@ -95,6 +103,8 @@ function fireStar(event) {
   console.log("Fire Star!");
   fungusHP -= 25;
   fungusAP -= 33;
+  hpMeter.value -= 25;
+  apMeter.value -= 33;
   if (fungusAP < 0) {
     fungusAP = 0;
   }
