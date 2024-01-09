0de9a0093804cf5af827848e9fbba37b3c14a7bb
Riley DuBord <thelifeofdu@gmail.com>
2024-01-08 19:00:58 -0600

    HP & AP have a min. of 0 now

--- script.js
+++ script.js
@@ -3,52 +3,77 @@
 // Example:
 let fungusHP = 100;
 let fungusAP = 100;
-let myAPElement = document.getElementsByClassName('ap-text')[0];
-let yourHPElement = document.getElementsByClassName('hp-text')[0];
-console.log(yourHPElement,myAPElement);
+let myAPElement = document.getElementsByClassName("ap-text")[0];
+let yourHPElement = document.getElementsByClassName("hp-text")[0];
+console.log(yourHPElement, myAPElement);
 
 function onReady() {
-    console.log("Ready to go!")
-    
-    // Make sure you check the index.html file! 
-    // There are lots of buttons and things ready for you to hook into here!
-    
-    
-    // 🧠 Remember
-    // - Handle events that ->
-    // - Updates state which is ->
-    // - Rendered to the DOM
+  console.log("Ready to go!");
+
+  // Make sure you check the index.html file!
+  // There are lots of buttons and things ready for you to hook into here!
+
+  // 🧠 Remember
+  // - Handle events that ->
+  // - Updates state which is ->
+  // - Rendered to the DOM
 }
 
-onReady()
+onReady();
 
-function renderFire(){
-
+function fireScepter(event) {
+  console.log("Fire Scepter!");
+  fungusHP -= 14;
+  fungusAP -= 12;
+  if (fungusAP < 0) {
+    fungusAP = 0;
+  }
+  if (fungusHP < 0) {
+    fungusHP = 0;
+  }
+  console.log(fungusAP, fungusHP);
+  myAPElement.innerHTML = `${fungusAP} AP`;
+  yourHPElement.innerHTML = `${fungusHP} HP`;
 }
 
-function fireScepter(event){
-    console.log('Fire Scepter!')
-    fungusHP -= 14
-    fungusAP -= 12
-    console.log(fungusAP, fungusHP)
-    myAPElement.innerHTML = `${fungusAP} AP`
-    yourHPElement.innerHTML = `${fungusHP} HP`
+function fireEntagle(event) {
+  console.log("Fire Entagle!");
+  fungusHP -= 9;
+  fungusAP -= 23;
+  if (fungusAP < 0) {
+    fungusAP = 0;
+  }
+  if (fungusHP < 0) {
+    fungusHP = 0;
+  }
+  myAPElement.innerHTML = `${fungusAP} AP`;
+  yourHPElement.innerHTML = `${fungusHP} HP`;
 }
 
-function fireEntagle(event){
-    console.log('Fire Entagle!')
-    fungusHP -= 9
-    fungusAP -= 23
+function fireBlade(event) {
+  console.log("Fire Blade!");
+  fungusHP -= 47;
+  fungusAP -= 38;
+  if (fungusAP < 0) {
+    fungusAP = 0;
+  }
+  if (fungusHP < 0) {
+    fungusHP = 0;
+  }
+  myAPElement.innerHTML = `${fungusAP} AP`;
+  yourHPElement.innerHTML = `${fungusHP} HP`;
 }
 
-function fireBlade(event){
-    console.log('Fire Blade!')
-    fungusHP -= 47
-    fungusAP -= 38
-}
-
-function fireStar(event){
-    console.log('Fire Star!')
-    fungusHP -= 25
-    fungusAP -= 33
+function fireStar(event) {
+  console.log("Fire Star!");
+  fungusHP -= 25;
+  fungusAP -= 33;
+  if (fungusAP < 0) {
+    fungusAP = 0;
+  }
+  if (fungusHP < 0) {
+    fungusHP = 0;
+  }
+  myAPElement.innerHTML = `${fungusAP} AP`;
+  yourHPElement.innerHTML = `${fungusHP} HP`;
 }
